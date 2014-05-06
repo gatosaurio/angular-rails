e52e36fc9c5e1a533d109e84b353ff32cbc6db12
gatosaurio <hermeskybalion@hotmail.com>
2014-05-06 18:57:25 -0400

    rails api to angular

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutsplusRails::Application.config.secret_token = 'b70e57acf285a2d88efe1028215b27a7edd88ca47cefbda85e3888aecfdb8f11ae877f3ba21b8568eeb6d81328f960a0eddf6f488621ae367c8840d870957756'
